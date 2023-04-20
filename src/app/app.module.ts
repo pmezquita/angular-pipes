d722282d9d79bcad3e9951a3538f933d0e49d7f5
pmezquita <ing.pedromezquita@gmail.com>
2023-04-20 11:11:20 -0600

    [ADD] primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
