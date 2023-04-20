b68330e394266237e0eff4b7bf37f9dd916faf60
pmezquita <ing.pedromezquita@gmail.com>
2023-04-20 13:16:43 -0600

    [ADD] Menubar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {PrimengModule} from "./primeng/primeng.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import {PrimengModule} from "./primeng/primeng.module";
   ],
   imports: [
     BrowserModule,
-    PrimengModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
