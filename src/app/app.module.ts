fa96e037e8e80aeb5fee0c7576b4b2f50176c9c2
pmezquita <ing.pedromezquita@gmail.com>
2023-04-21 10:47:46 -0600

    [ADD] set Locale global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {VentasModule} from "./ventas/ventas.module";
+
+// Cambiar el locale de la app
+import localeEsMx from '@angular/common/locales/es-MX';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localeEsMx);
 
 @NgModule({
   declarations: [
@@ -16,7 +22,12 @@ import {VentasModule} from "./ventas/ventas.module";
     AppRoutingModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide: LOCALE_ID,
+    useValue: 'es-MX',
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
