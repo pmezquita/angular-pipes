01c61fb99b3b3318b9f242d16b9eeedb8bea7524
pmezquita <ing.pedromezquita@gmail.com>
2023-04-21 12:41:02 -0600

    [ADD] i18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,12 +7,20 @@ import {Component} from '@angular/core';
 })
 export class NoComunesComponent {
 
+  // i18nSelect
   nombre: string = 'Pedro';
   genero: string = 'masculino';
-
   invitacionMap = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla',
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan'];
+  clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
   }
 
 }
