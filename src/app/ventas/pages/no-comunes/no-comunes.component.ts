0fd30822a37b912f4a7722163cf0c6092a2ce46b
pmezquita <ing.pedromezquita@gmail.com>
2023-04-21 12:30:33 -0600

    [ADD] i18nSelectPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class NoComunesComponent {
+
+  nombre: string = 'Pedro';
+  genero: string = 'masculino';
+
+  invitacionMap = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla',
+  }
 
 }
